23c3ad98ec70ee98a3d7fb85ed56838da37cce4a
Igli2 <sg.gerdon@gmx.de>
2023-04-21 11:30:51 +0200

    readded table search

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import sys
-
-from PyQt5.QtWidgets import QHBoxLayout
-
 from graph.stock_graph import StockGraph
 from logs.log import log_message
 from stock_data.stocks import Stonks, Stock
 from user.user import User
 from PyQt5 import uic, QtWidgets, QtGui, QtCore
 from worker.table import StockTableItemWorker
+from enum import Enum
+import re
 
-from stock_data.stock_page import StockPage
+
+class StockTableColumn(Enum):
+    INFO = 0
+    ID = 1
+    NAME = 2
+    PRICE = 3
+    DIFF = 4
+    YIELD = 5
+    GRAPH = 6
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -48,6 +55,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.loader_thread.finished.connect(self.loader_thread.deleteLater)
 
         self.loader_thread.start()
+        log_message("Started loading stock details")
 
     def set_stock_details(self, stock_id):
         stock_name = self.findChild(QtWidgets.QLabel, "stockName")
@@ -64,7 +72,7 @@ class MainWindow(QtWidgets.QMainWindow):
             for i in reversed(range(layout.count())):
                 layout.itemAt(i).widget().setParent(None)
         else:
-            layout = QHBoxLayout()
+            layout = QtWidgets.QHBoxLayout()
             stock_graph.setLayout(layout)
         graph = StockGraph(target_stock.get_time_stamps(365), target_stock.get_prices(365))
         layout.addWidget(graph.get_widget())
@@ -85,6 +93,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def on_stock_table_load_finished(self):
         info_label: QtWidgets.QLabel = self.findChild(QtWidgets.QLabel, "stockTableLoadingIndicator")
         info_label.setText("")
+        log_message("Finished loading stock details")
 
     def stock_table_append(self, stock: Stock):
         stock_table: QtWidgets.QTableWidget = self.findChild(QtWidgets.QTableWidget, "stockDetailTable")
@@ -100,19 +109,22 @@ class MainWindow(QtWidgets.QMainWindow):
         i = stock_table.rowCount()
         stock_table.insertRow(i)
 
-        stock_table.setCellWidget(i, 0, info_widget)
-        stock_table.setCellWidget(i, 1, QtWidgets.QLabel(f"{stock.get_name()}"))
-        stock_table.setCellWidget(i, 2, QtWidgets.QLabel(f"{stock.get_long_name()}"))
-        stock_table.setCellWidget(i, 3,
+        stock_table.setCellWidget(i, StockTableColumn.INFO.value, info_widget)
+        stock_table.setCellWidget(i, StockTableColumn.ID.value, QtWidgets.QLabel(f"{stock.get_name()}"))
+        stock_table.setCellWidget(i, StockTableColumn.NAME.value, QtWidgets.QLabel(f"{stock.get_long_name()}"))
+        stock_table.setCellWidget(i, StockTableColumn.PRICE.value,
                                     QtWidgets.QLabel(f"{stock.get_ask_price()} {stock.get_currency()}"))
-        stock_table.setCellWidget(i, 4, QtWidgets.QLabel(f"({sign}{'{:4.2f}'.format(stock_trend)}%)"))
-        stock_table.setCellWidget(i, 5, stock_graph.get_widget())
+        stock_table.setCellWidget(i, StockTableColumn.DIFF.value, QtWidgets.QLabel(f"({sign}{'{:4.2f}'.format(stock_trend)}%)"))
+        stock_table.setCellWidget(i, StockTableColumn.GRAPH.value, stock_graph.get_widget())
+        stock_table.setCellWidget(i, StockTableColumn.YIELD.value, QtWidgets.QLabel(f"{round(stock.get_dividend_yield() * 100, 4)}%"))
+
+        self.on_search_changed()
 
     def show_stock_info(self):
         table: QtWidgets.QTableWidget = self.findChild(QtWidgets.QTableWidget, "stockDetailTable")
         row = table.currentRow()
 
-        stock_id = table.cellWidget(row, 1).text()
+        stock_id = table.cellWidget(row, StockTableColumn.ID.value).text()
         self.set_stock_details(stock_id)
 
         tab_widget = self.findChild(QtWidgets.QTabWidget, "tabWidget")
@@ -124,6 +136,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.findChild(QtWidgets.QSlider, "stockPriceFilter2").valueChanged.connect(self.on_search_changed)
         self.findChild(QtWidgets.QCheckBox, "positiveTrendsCheck").stateChanged.connect(self.on_search_changed)
         self.findChild(QtWidgets.QCheckBox, "negativeTrendsCheck").stateChanged.connect(self.on_search_changed)
+        self.findChild(QtWidgets.QSlider, "stockYieldFilter").valueChanged.connect(self.on_search_changed)
         self.findChild(QtWidgets.QPushButton, "loginButton").clicked.connect(self.on_login_button)
         self.findChild(QtWidgets.QAction, "actionExit").triggered.connect(self.on_exit)
         self.findChild(QtWidgets.QPushButton, "logoutButton").clicked.connect(self.on_logout)
@@ -210,6 +223,7 @@ class MainWindow(QtWidgets.QMainWindow):
         value_upper = self.findChild(QtWidgets.QSlider, "stockPriceFilter2").value()
         positive_check = self.findChild(QtWidgets.QCheckBox, "positiveTrendsCheck").isChecked()
         negative_check = self.findChild(QtWidgets.QCheckBox, "negativeTrendsCheck").isChecked()
+        yield_slider_value = self.findChild(QtWidgets.QSlider, "stockYieldFilter").value()
 
         if value_lower > value_upper:
             slider = self.findChild(QtWidgets.QSlider, "stockPriceFilter2")
@@ -220,7 +234,18 @@ class MainWindow(QtWidgets.QMainWindow):
             slider.setValue(value_upper - 1)
             value_lower -= 1
 
-        # TODO: new search algorithm for stock data pages
+        stock_table: QtWidgets.QTableWidget = self.findChild(QtWidgets.QTableWidget, "stockDetailTable")
+        for i in range(stock_table.rowCount()):
+            name = stock_table.cellWidget(i, StockTableColumn.NAME.value).text()
+            price_text = stock_table.cellWidget(i, StockTableColumn.PRICE.value).text()
+            price = float(re.findall("\d+\.\d+", price_text)[0])
+            diff = stock_table.cellWidget(i, StockTableColumn.DIFF.value).text()
+            stock_yield = float(stock_table.cellWidget(i, StockTableColumn.YIELD.value).text()[:-1])
+            if search.lower() not in name.lower() or price < value_lower or price > value_upper or \
+                ("+" in diff and not positive_check) or ("-" in diff and not negative_check) or stock_yield < yield_slider_value:
+                    stock_table.hideRow(i)
+            else:
+                stock_table.showRow(i)
 
     def set_icons(self):
         self.setWindowIcon(QtGui.QIcon("assets/logo.png"))
